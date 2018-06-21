171ded6d476223bf3cc0fdf9084a48725b72cd92
scgakki <1833503372@qq.com>
2018-06-21 16:43:38 +0800

    the first

---
+++ helloclass.h
@@ -0,0 +1,16 @@
+#ifndef HELLOCLASS_H
+#define HELLOCLASS_H
+
+#include <iostream>
+using namespace std;
+
+class helloclass
+{
+public:
+    helloclass();
+    ~helloclass();
+    void sayhello();
+};
+
+#endif
+       
