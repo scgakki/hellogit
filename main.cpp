06b2a6415ee67d3bd37aa364e9e1a0233b56f859
scgakki <1833503372@qq.com>
2018-06-21 16:46:56 +0800

    the second

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include<iostream>
+#include "helloclass.h"
 using namespace std;
 
 int main()
 {
-    cout<<"HELLO GIT"<<endl;
-    return 0;
+   helloclass HelloClass;
+   HelloClass.sayhello();
+   return 0;
 }
-
